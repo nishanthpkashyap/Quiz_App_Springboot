1f10d8a1c6542cf1baf40a63423dcdf95f6ab936
nishanth <nishanthpkashyap1503@gmail.com>
2023-07-02 02:56:56 +0530

    added QuestionService.java

--- src/main/java/com/nishanthpkashyap/quizapp/controller/QuestionController.java
+++ src/main/java/com/nishanthpkashyap/quizapp/controller/QuestionController.java
@@ -1,15 +1,21 @@
 package com.nishanthpkashyap.quizapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.nishanthpkashyap.quizapp.service.QuestionService;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
 
+    @Autowired
+    private QuestionService questionService;
+
     @RequestMapping(value = "/allQuestions", method = RequestMethod.GET)
     public String getAllQuestions() {
-        return "Hello, newbie";
+//        return "Hello, newbie";
+          return questionService.getAllQuestions();
     }
 }
---
+++ src/main/java/com/nishanthpkashyap/quizapp/service/QuestionService.java
@@ -0,0 +1,10 @@
+package com.nishanthpkashyap.quizapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+    public String getAllQuestions(){
+        return "Here I come...";
+    }
+}
