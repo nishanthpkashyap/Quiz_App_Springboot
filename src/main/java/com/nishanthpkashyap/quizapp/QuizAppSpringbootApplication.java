006bfa6c3085c117ab54cd9f358e9ca526b91138
nishanth <nishanthpkashyap1503@gmail.com>
2023-07-02 02:15:37 +0530

    initial commit

---
+++ src/main/java/com/nishanthpkashyap/quizapp/QuizAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nishanthpkashyap.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nishanthpkashyap/quizapp/QuizAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishanthpkashyap.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
